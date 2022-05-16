869efd6dacd19321aeefe9f9d7c4590fbd16af60
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-05-17 00:14:26 +0300

    method for joining with any model if that have image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Query\JoinClause;
 
 class Image extends Model
 {
@@ -17,5 +19,21 @@ class Image extends Model
     public function morph_image(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public static function imageJoin(string $model, string $table, string $column = null, string $operator = null, int $days = null)
+    {
+        $date = Carbon::now()->addDays($days)?->format('Y-m-d H:i:s');
+        $query = $model::with('images')->select("$table.*");
+        if(isset($date, $column, $operator)){
+            $query->where("$table.$column", "$operator", "'$date'");
+        }
+
+        $query->leftJoin('images', function (JoinClause $join) use ($model, $table) {
+            $join->on('images.imageble_id', '=', "$table.id");
+            $join->where('images.imageble_type', '=', "$model");
+        })->addSelect('images.img_source as images');
+
+        return $query;
     }
 }
